5f9c2dfdf4283d34611186b18550134133ca4791
Gustavo Kesley Fontes <72459288+gustavo-f0ntz@users.noreply.github.com>
2023-10-12 12:00:16 -0300

    Criando a estrutura da tabela de tarefas

--- src/main/java/br/com/gustavof0ntz/todolist/task/TaskModel.java
+++ src/main/java/br/com/gustavof0ntz/todolist/task/TaskModel.java
@@ -1,5 +1,7 @@
 package br.com.gustavof0ntz.todolist.task;
 
+import java.time.LocalDateTime;
+
 public class TaskModel {
     /**
      * ID da tarefa
@@ -10,6 +12,15 @@ public class TaskModel {
      * Data de Termino da tarefa
      * Prioridade
      * 
-     * 
      */
+
+     private UUID id;
+     private String description;
+     private String title;
+     private LocalDateTime startAt;
+     private LocalDateTime endAt;
+     private String priority;
+
+     private LocalDateTime createdAt;
+     
 }
