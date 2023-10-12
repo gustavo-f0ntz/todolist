8b83a7dcb864e3fc76722c04ef45cab109eb0102
Gustavo Kesley Fontes <72459288+gustavo-f0ntz@users.noreply.github.com>
2023-10-12 11:48:36 -0300

    Initial commit

---
+++ src/main/java/br/com/gustavof0ntz/todolist/task/TaskModel.java
@@ -0,0 +1,15 @@
+package br.com.gustavof0ntz.todolist.task;
+
+public class TaskModel {
+    /**
+     * ID da tarefa
+     * Usuario (ID_USUARIO)
+     * Descricao
+     * Titulo
+     * Data de inicio
+     * Data de Termino da tarefa
+     * Prioridade
+     * 
+     * 
+     */
+}
---
+++ src/test/java/br/com/gustavof0ntz/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavof0ntz.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
