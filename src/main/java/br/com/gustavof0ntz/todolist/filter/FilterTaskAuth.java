18499c23366a5b3b4abe2e9fe44c11368f9b022a
Gustavo Kesley Fontes <72459288+gustavo-f0ntz@users.noreply.github.com>
2023-10-13 09:52:01 -0300

    Implementando um Filtro de Autenticação de Tarefas

---
+++ src/main/java/br/com/gustavof0ntz/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,26 @@
+package br.com.gustavof0ntz.todolist.filter;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+@Component
+public class FilterTaskAuth implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+       
+                //Pedir para realizar alguma ação ou seguir rota
+
+                System.out.println("Chegou no Filtro");
+                chain.doFilter(request, response);
+    }
+    
+}
