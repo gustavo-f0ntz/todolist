2be8e9c94e632073dad7a420d6120ce8855b9e75
Gustavo Kesley Fontes <72459288+gustavo-f0ntz@users.noreply.github.com>
2023-10-13 11:50:28 -0300

    Atualizando o filtro

--- src/main/java/br/com/gustavof0ntz/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/gustavof0ntz/todolist/filter/FilterTaskAuth.java
@@ -3,9 +3,12 @@ package br.com.gustavof0ntz.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.gustavof0ntz.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,6 +16,11 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
+
+
+    @Autowired
+    private IUserRepository userRepository;
+
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -33,15 +41,28 @@ public class FilterTaskAuth extends OncePerRequestFilter {
             String[] credentials =  authString.split(":");
             String username = credentials[0];
             String password = credentials[1];
-            System.out.println("Authorization");
-            System.out.println(username);
-            System.out.println(password);
+            
+
 
 
                 // Validar usuario
-                // Validar senha
-                // Segue rota
+                    var user = this.userRepository.findByUsername(username);
+                    if (user == null) {
+                       response.sendError(401);
+                    } else {
+                          // Validar senha
+                        var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                        if(passwordVerify.verified) {
+                            filterChain.doFilter(request, response);
+                        }else {
+                            response.sendError(401);
+                        }
 
+                         // Segue rota
+
+                    }
+
+              
 
 
 
