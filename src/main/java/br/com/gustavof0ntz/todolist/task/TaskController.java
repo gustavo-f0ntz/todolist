f02d6d25b7becc10a8a88bfa9b2e0e9f19356258
Gustavo Kesley Fontes <72459288+gustavo-f0ntz@users.noreply.github.com>
2023-10-15 11:36:52 -0300

    Updated Nas TaskLists e Validações

--- src/main/java/br/com/gustavof0ntz/todolist/task/TaskController.java
+++ src/main/java/br/com/gustavof0ntz/todolist/task/TaskController.java
@@ -57,14 +57,27 @@ public class TaskController {
 
     // para passar o endereço da task e o Id
     @PutMapping("/{id}")
-    public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+    public ResponseEntity update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
 
 
         var task = this.taskRepository.findById(id).orElse(null);
 
+        if(task == null) {
+             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body("Tarefa não encontrada!");
+        }
+
+        var idUser = request.getAttribute("idUser");
+
+        if(!task.getIdUser().equals(idUser)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body("Usuário não tem permissão para alterar essa tarefa.");
+        }
+
         Utils.copyNonNullProperties(taskModel, task);
 
-        
-        return this.taskRepository.save(task);
+        var taskUpdated = this.taskRepository.save(task);
+        return ResponseEntity.ok().body(taskUpdated);
+
     }
 }
